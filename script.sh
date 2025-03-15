491a0e21ac907fcaea1414098090be338212648c
Aina Rajaonah Andriamanalimanda <aandriam@c1r4s13.42antananarivo.mg>
2025-03-15 22:56:46 +0300

    test

--- script.sh
+++ script.sh
@@ -1,6 +1,33 @@
 #!/bin/bash
-echo "check for [oh my zsh]"
+clear
+echo "[ ]checking for [oh my zsh]"
+echo "[ ]checking for [command_sh]"
+echo "[ ]checking for [kitty Binary]"
+echo "[ ]checking for [kitty config]"
 if [ ! -d "$HOME/.oh-my-zsh" ]; then
     sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
-fi
-echo "[✅]Oh My Zsh installed !"
+clear
+echo "[✅]ok: [Oh My Zsh] !"
+echo "[ ]checking for [command_sh]"
+echo "[ ]checking for [kitty Binary]"
+echo "[ ]checking for [kitty config]"
+rm -rf ~/command_sh/
+git clone git@github.com:aandriamgit/command_sh.git ~/command_sh
+clear
+echo "[✅]ok: [Oh My Zsh]"
+echo "[✅]ok: [command_sh]"
+echo "[ ]checking for [kitty Binary]"
+echo "[ ]checking for [kitty config]"
+curl -L https://sw.kovidgoyal.net/kitty/installer.sh | sh /dev/stdin
+clear
+echo "[✅]ok: [Oh My Zsh]"
+echo "[✅]ok: [command_sh]"
+echo "[✅]ok: [kitty Binary]"
+echo "[ ]checking for [kitty config]"
+rm -rf ~/.config/kitty/
+git clone git@github.com:aandriamgit/kitty.git ~/.config/kitty
+clear
+echo "[✅]ok: [Oh My Zsh]"
+echo "[✅]ok: [command_sh]"
+echo "[✅]ok: [kitty Binary]"
+echo "[✅]ok: [kitty config]"
